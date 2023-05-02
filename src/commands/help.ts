51bde360112fd1bb5ef255af934d40e381205aea
Paphon <Paphon_work@hotmail.com>
2023-05-02 12:26:45 +0700

    update fix .getString in /help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { ChannelType, Client, CommandInteraction, TextChannel } from "discord.js";
+import { ChannelType, ChatInputCommandInteraction, Client, CommandInteraction, TextChannel } from "discord.js";
 
 export const data = new SlashCommandBuilder()
     .setName("help")
@@ -11,7 +11,9 @@ export const data = new SlashCommandBuilder()
         .setRequired(true)
     );
 
-export async function execute(interaction: CommandInteraction, client: Client) {
+export async function execute(interaction: ChatInputCommandInteraction, client: Client) {
+    console.log("yoyo1");
+    
     if(!interaction?.channelId){
         return;
     }
@@ -19,18 +21,21 @@ export async function execute(interaction: CommandInteraction, client: Client) {
     if(!channel || channel.type !== ChannelType.GuildText){
         return;
     }
+    console.log("yoyo2");
     const thread = await (channel as TextChannel).threads.create({
         name: `suport-${Date.now}`,
         reason: `Support ticket ${Date.now} created by ${interaction.user.tag}`
     })
- 
+    console.log("yoyo3");
     const problemsDescription = interaction.options.getString("description");
     const { user } = interaction;
-    thread.send{`${user} has a problem: ${problemsDescription}`};
+   // thread.send{`${user} has a problem: ${problemsDescription}`};
 
     //TODO: send ticket to firebase
+    //TODO: fix throw error on /help
 
     return interaction.reply({
         content: `Your ticket has been created`,
         ephemeral: true
     });
+}
