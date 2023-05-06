d18434a82d70e644b0f147256207258c510f2093
Paphon <Paphon_work@hotmail.com>
2023-05-06 23:14:42 +0700

    - using axios to get cat pics - still need to resolve the "undefined" data in /catpic

--- src/commands/catpic.ts
+++ src/commands/catpic.ts
@@ -1,12 +1,42 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
+import axios from "axios";
+import { json } from "express";
 
 export const data = new SlashCommandBuilder()
     .setName("catpic")
-    .setDescription("Replies with cat picture!");
+    .setDescription("Replies with random cat picture!");
+
+type Cat = {
+    "id": string,
+    "url": string,
+    "width": number,
+    "height": number
+    "breeds": [],
+    "favorite": object
+}
+
+type getCatPicResponse = {
+    "data": Cat
+}
+
+
+// use thecatapi to get random cat pictures! (using axios to get data)
+async function getCatPic(): Promise<Cat> {
+    try{
+        const url = "https://api.thecatapi.com/v1/images/search";
+        const response = await axios.get<getCatPicResponse>(url);
+        //console.log(response.data);
+        const cat = response.data.data;
+        return cat;
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
+}
 
 export async function execute(interaction: CommandInteraction) {
-    //let cat = await fetch("https://api.thecatapi.com/v1/images/search")
-    //console.log(cat);
-    await interaction.reply("catpic!");
+    let catGet = await getCatPic();
+    console.log(catGet);
+    await interaction.reply("cat.url");
 }
